d20e6a7e09c23807b514843f13ba575a603351dc
Gene Liverman <gene.liverman+06301990@puppet.com>
2019-03-21 12:43:42 -0400

    Updated to parse more data and refactored a bit

--- tintri_graphite.py
+++ tintri_graphite.py
@@ -23,6 +23,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+import copy
 import json
 import os
 import socket
@@ -64,20 +65,13 @@ def print_error(out):
     return
 
 
-def get_vmstore(session_id):
-
-    # Dictionary of VMstore objects
-    vmstore_stats = {}
-
-    # URL for VMstore stats
-    get_vmstore_url = "/v310/datastore/default/statsRealtime"
-    count = 1
-
+def get_datastore_stats(session_id, stats):
+    get_datastore_stats_base_url = "/v310/datastore/default/"
+    stats_url = get_datastore_stats_base_url + stats
     # Invoke the API
-    r = tintri.api_get(server_name, get_vmstore_url, session_id)
+    r = tintri.api_get(server_name, stats_url, session_id)
     print_debug("The JSON response of the get invoke to the server " +
                 server_name + " is: " + r.text)
-
     # if HTTP Response is not 200 then raise an error
     if r.status_code != 200:
         print_error("The HTTP response for the get invoke to the server " +
@@ -86,13 +80,91 @@ def get_vmstore(session_id):
         print_error("response: " + r.text)
         tintri.api_logout(server_name, session_id)
         sys.exit(-10)
-
     # Get and store the VM items and save in a vmstore object.
     vmstore_result = r.json()
+    return vmstore_result
 
-    vmstore_stats = vmstore_result["items"][0]["sortedStats"]
 
+# Copyright Ferry Boender, released under the MIT license.
+def deepupdate(target, src):
+    """Deep update target dict with src
+    For each k,v in src: if k doesn't exist in target, it is deep copied from
+    src to target. Otherwise, if v is a list, target[k] is extended with
+    src[k]. If v is a set, target[k] is updated with v, If v is a dict,
+    recursively deep-update it.
+    Examples:
+    >>> t = {'name': 'Ferry', 'hobbies': ['programming', 'sci-fi']}
+    >>> deepupdate(t, {'hobbies': ['gaming']})
+    >>> print t
+    {'name': 'Ferry', 'hobbies': ['programming', 'sci-fi', 'gaming']}
+    """
+    for k, v in src.items():
+        if type(v) == list:
+            if not k in target:
+                target[k] = copy.deepcopy(v)
+            else:
+                target[k].extend(v)
+        elif type(v) == dict:
+            if not k in target:
+                target[k] = copy.deepcopy(v)
+            else:
+                deepupdate(target[k], v)
+        elif type(v) == set:
+            if not k in target:
+                target[k] = v.copy()
+            else:
+                target[k].update(v.copy())
+        else:
+            target[k] = copy.copy(v)
+
+
+def get_vmstore(session_id):
+    # Dictionary of VMstore objects
+    vmstore_stats = {}
+    # URLs for VMstore stats
+    stats_summary = "statsSummary"
+    stats_realtime = "statsRealtime"
+    vmstore_stats = get_datastore_stats(session_id, stats_summary)
+    realtime_stats_obj = get_datastore_stats(session_id, stats_realtime)
+    realtime_stats = realtime_stats_obj["items"][0]["sortedStats"][0]
+    deepupdate(vmstore_stats,realtime_stats)
     return vmstore_stats
+
+
+def graphite_message(prefix, stat, value, epochtime):
+    message = '%s.%s %f %d\n' % (prefix, stat, float(value), epochtime)
+    return message
+
+
+def send_graphite_metric(server, port, message):
+    # Send stat to graphite
+    graphite_socket = socket.socket()
+    graphite_socket.connect((server, port))
+    graphite_socket.sendall(message)
+    graphite_socket.close()
+
+
+def parse_stat(prefix, parent, stat, epochtime):
+    if type(parent[stat]) is dict:
+        for substat in parent[stat]:
+            new_prefix = '%s.%s' % (prefix, stat)
+            parse_stat(new_prefix, parent[stat], substat, epochtime)
+    elif (type(parent[stat]) is float) or (type(parent[stat]) is int) or (type(parent[stat]) is bool):
+        message = graphite_message(prefix, stat, parent[stat], epochtime)
+        print_debug(message)
+        send_graphite_metric(graphite_server, graphite_port, message)
+    else:
+        # Skip stat if it's a non-number.  Probably need to add a nested loop above to handle type dict
+        print_debug("%s: %s is a %s instead of a float\n" % (stat, parent[stat], type(parent[stat])))
+
+
+def print_graphite_connection_info():
+    print_info("VMstore: %s" % (server_name))
+    print_info("API Version: " + json_info['preferredVersion'])
+    print_info("Update Frequency: %s seconds" % (interval))
+    print_info("Graphite Server: %s" % (graphite_server))
+    print_info("Graphite Port: %s" % (graphite_port))
+    print_info("Graphite storage schema: tintri.%s." % (graphite_formatted_server_name))
 
 
 # main
@@ -115,18 +187,13 @@ else:
     print("Usage: " + sys.argv[0] + " vmstore_fqdn vmstore_username vmstore_password graphite_fqdn graphite_port\n")
     sys.exit(-1)
 
-server_name_formatted = server_name.replace(".", "_")
+graphite_formatted_server_name = server_name.replace(".", "_")
 
 # Get the preferred version
 r = tintri.api_version(server_name)
 json_info = r.json()
 
-print_info("VMstore: %s" % (server_name))
-print_info("API Version: " + json_info['preferredVersion'])
-print_info("Update Frequency: %s seconds" % (interval))
-print_info("Graphite Server: %s" % (graphite_server))
-print_info("Graphite Port: %s" % (graphite_port)) 
-print_info("Graphite storage schema: tintri.%s." % (server_name_formatted))
+print_graphite_connection_info
 
 while True:
     print_debug("Starting VMstore collection...")
@@ -134,33 +201,20 @@ while True:
     # Login to VMstore
     session_id = tintri.api_login(server_name, user_name, password)
 
+    # Gather stats
     vmstore_stats = get_vmstore(session_id)
 
     # Get epoch time
     epochtime = int(time.time())
 
-    # All pau, log out
+    # All done, log out
     tintri.api_logout(server_name, session_id)
 
-    for stat in vmstore_stats[0]:
-        # Make sure each stat is a number (float or int)
-        if (type(vmstore_stats[0][stat]) is float) or (type(vmstore_stats[0][stat]) is int):
-            message = 'tintri.%s.vmstore.%s %f %d\n' % (server_name_formatted, stat, float(vmstore_stats[0][stat]), epochtime)
-            print_debug(message)
-
-            # Send stat to graphite
-            sock = socket.socket()
-            sock.connect((graphite_server, graphite_port))
-            sock.sendall(message)
-
-	else:
-            # Skip stat if it's a non-number.  Probably need to add a nested loop above to handle type dict
-            print_debug("%s: %s is a %s instead of a float\n" % (stat, vmstore_stats[0][stat], type(vmstore_stats[0][stat])))
+    for stat in vmstore_stats:
+        prefix = 'tintri.' + graphite_formatted_server_name + '.vmstore'
+        parse_stat(prefix, vmstore_stats, stat, epochtime)
 
     print_debug("Collection finished")
-
-    # Close graphite socket
-    sock.close()
 
     # Wait for x seconds
     print_debug("Sleeping for %s seconds..." % (interval))
